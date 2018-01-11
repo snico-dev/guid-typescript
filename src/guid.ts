2aaca61f0757363ac494e01fcc5d01e329ca8a85
Nicolas <nicolas@vitrio.com.br>
2018-01-11 12:30:25 -0200

    init

---
+++ src/guid.ts
@@ -0,0 +1,70 @@
+export class Guid {
+
+    public static validator = new RegExp("^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$", "i");
+
+    public static EMPTY = "00000000-0000-0000-0000-000000000000";
+
+    public static isGuid(guid: any) {
+        const value: string = guid.toString();
+        return guid && (guid instanceof Guid || Guid.validator.test(value));
+    }
+
+    public static create() {
+        return new Guid([Guid.gen(2), Guid.gen(1), Guid.gen(1), Guid.gen(1), Guid.gen(3)].join("-"));
+    }
+
+    public static createEmpty() {
+        return new Guid("emptyguid");
+    }
+
+    public static parse(guid: string) {
+        return new Guid(guid);
+    }
+
+    public static raw() {
+        return [Guid.gen(2), Guid.gen(1), Guid.gen(1), Guid.gen(1), Guid.gen(3)].join("-");
+    }
+
+    private static gen(count: number) {
+        let out: string = "";
+        for (let i: number = 0; i < count; i++) {
+            // tslint:disable-next-line:no-bitwise
+            out += (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+        }
+        return out;
+    }
+
+    private value: string;
+
+    private constructor(guid: string) {
+        if (!guid) { throw new TypeError("Invalid argument; `value` has no value."); }
+
+        this.value = Guid.EMPTY;
+
+        if (guid && Guid.isGuid(guid)) {
+            this.value = guid;
+        }
+    }
+
+    public equals(other: Guid): boolean {
+        // Comparing string `value` against provided `guid` will auto-call
+        // toString on `guid` for comparison
+        return Guid.isGuid(other) && this.value === other.toString();
+    }
+
+    public isEmpty(): boolean {
+        return this.value === Guid.EMPTY;
+    }
+
+    public toString(): string {
+        return this.value;
+    }
+
+    public toJSON(): any {
+        return {
+            value: this.value,
+        };
+    }
+}
+
+
---
+++ tests/guid.spec.ts
@@ -0,0 +1,55 @@
+import { assert, expect } from "chai";
+import "mocha";
+import { Guid } from "../src/guid";
+
+describe("Guid test", () => {
+    
+    it("Should create a guid", () => {
+        const wrong = "wrongguid";
+        expect(Guid.isGuid(wrong)).equal(false);
+
+        const right = Guid.create();
+        expect(Guid.isGuid(right)).equal(true);
+    });
+
+    it("Should raw a guid", () => {
+        const wrong = "wrongguid";
+        expect(Guid.isGuid(wrong)).equal(false);
+
+        const right = Guid.raw();
+        expect(Guid.isGuid(right)).equal(true);
+    });
+
+    it("Should compare another guid", () => {
+        const wrong = Guid.create();
+        expect(wrong.equals(Guid.create())).equal(false);
+        
+        const right = Guid.create();
+        expect(right.equals(right)).equal(true);
+    });
+
+    it("Should compare another guid empty", () => {
+        const wrong = Guid.createEmpty(); 
+        expect(wrong.equals(Guid.create())).equal(false);
+        
+        const right = Guid.createEmpty();
+        expect(right.equals(Guid.createEmpty())).equal(true);
+    });
+
+    it("Should verify if is guid", () => {
+        const wrong = "wrong guid";
+        expect(Guid.isGuid(wrong)).equal(false);
+        
+        const right = Guid.create();
+        expect(Guid.isGuid(right)).equal(true);
+    });
+
+    it("Should parse a guid", () => {
+        const wrong = Guid.raw();
+        expect(Guid.parse(wrong).equals(Guid.create())).equal(false);
+        
+        const right = Guid.raw();
+        expect(Guid.parse(right).equals(Guid.parse(right))).equal(true);
+    });
+
+});
