cbc8f61de8d25e63d562efbf288de31d014a8e80
gekkedev <17025257+gekkedev@users.noreply.github.com>
2019-06-25 17:24:17 +0200

    improved guid test accuracy
    The comparison tests were not properly covering the function to test.

--- tests/guid.spec.ts
+++ tests/guid.spec.ts
@@ -5,7 +5,7 @@ import { Guid } from "../lib/guid";
 describe("Guid", () => {    
     const exampleGuid = "0315642c-a069-9f3e-1852-9adf2d075b93";
 
-    it("Should create & validate a random guid", () => {
+    it("should create & validate a random guid", () => {
         const wrong = "wrongguid";
         expect(Guid.isGuid(wrong)).equal(false);
 
@@ -13,22 +13,23 @@ describe("Guid", () => {
         expect(Guid.isGuid(right)).equal(true);
     });
 
-    it("Should parse & validate a guid", () => {
+    it("should parse & validate a guid", () => {
         const wrong = "wrongguid";
         expect(Guid.isGuid(wrong)).equal(false);
 
         expect(Guid.isGuid(exampleGuid)).equal(true);
     });
 
-    it("Should compare to another guid", () => {
+    it("should compare GUID instances to another", () => {
         const wrong = Guid.create();
         expect(wrong.equals(Guid.create())).equal(false);
         
-        const right = Guid.create();
-        expect(right.equals(right)).equal(true);
+        const right = Guid.create(exampleGuid);
+        const duplicate = Guid.create(exampleGuid);
+        expect(right.equals(duplicate)).equal(true);
     });
 
-    it("Should be unique value", () => {
+    it("should be unique value", () => {
         const guids = [];
         for (let index = 0; index < 3000; index++) {
             guids.push(Guid.create());
@@ -38,11 +39,11 @@ describe("Guid", () => {
         expect(guids.indexOf(Guid.create()) < 0).equal(true);
     });
 
-    it("Should create nulled GUIDs & return them as string", () => {
+    it("should create nulled GUIDs & return them as string", () => {
         expect(Guid.createEmpty().toString()).equal(Guid.EMPTY);
     });
 
-    it("Should return valid JSON", () => {
+    it("should return valid JSON", () => {
         expect(Guid.create(exampleGuid).toJSON()).to.eql({ value: exampleGuid });
     });
 });
