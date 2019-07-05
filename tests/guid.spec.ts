76622fffe785718c9115da63c118a964d789d69c
gekkedev <17025257+gekkedev@users.noreply.github.com>
2019-07-05 11:33:18 +0200

    implemented types for test spec
    we want stable tests, eh?

--- tests/guid.spec.ts
+++ tests/guid.spec.ts
@@ -6,32 +6,32 @@ describe("Guid", () => {
     const exampleGuid: string = "0315642c-a069-9f3e-1852-9adf2d075b93";
 
     it("should create & validate a random guid", () => {
-        const wrong = "wrongguid";
+        const wrong: string = "wrongguid";
         expect(Guid.isValid(wrong)).equal(false);
 
-        const right = Guid.create();
+        const right: Guid = Guid.create();
         expect(Guid.isValid(right)).equal(true);
     });
 
     it("should parse & validate a guid", () => {
-        const wrong = "wrongguid";
+        const wrong: string = "wrongguid";
         expect(Guid.isValid(wrong)).equal(false);
 
         expect(Guid.isValid(exampleGuid)).equal(true);
     });
 
     it("should compare GUID instances to another", () => {
-        const wrong = Guid.create();
+        const wrong: Guid = Guid.create();
         expect(wrong.equals(Guid.create())).equal(false);
         
-        const right = Guid.create(exampleGuid);
-        const duplicate = Guid.create(exampleGuid);
+        const right: Guid = Guid.create(exampleGuid);
+        const duplicate: Guid = Guid.create(exampleGuid);
         expect(right.equals(duplicate)).equal(true);
     });
 
     it("should be unique value", () => {
-        const guids = [];
-        for (let index = 0; index < 3000; index++) {
+        const guids: Array<Guid> = [];
+        for (let index: number = 0; index < 3000; index++) {
             guids.push(Guid.create());
         }
         expect(guids.indexOf(guids[0]) < 0).equal(false);
